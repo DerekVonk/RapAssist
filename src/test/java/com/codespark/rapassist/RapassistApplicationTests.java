d3609856ca41ee413afa439a3dcf57f59b49d373
Derek Vonk <derek.vonk@hva.nl>
2019-01-26 15:24:16 +0100

    Initial spring boot generated project

---
+++ src/test/java/com/codespark/rapassist/RapassistApplicationTests.java
@@ -0,0 +1,17 @@
+package com.codespark.rapassist;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RapassistApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
